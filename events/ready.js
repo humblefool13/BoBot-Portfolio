ba0ff2b2bf0c3b331b93790a53f5f678d811ede6
humblefool13 <85033530+humblefool13@users.noreply.github.com>
2022-05-27 19:45:40 +0000

    subscription end informing

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,43 @@ const Discord = require('discord.js');
 const config_records = require('../models/configRecords');
 const sub_records = require('../models/subscriptionRecords');
 
+async function editEndMsg(config, client) {
+  const dm = config.dm;
+  if (dm) {
+    const user = await client.users.fetch(config.discord_id);
+    const channel = await user.createDM([true]);
+    const messages = config.message_ids;
+    for (i = 0; i <= 3; i++) {
+      const message = await channel.messages.fetch(messages[i]);
+      await message.edit({
+        content: ".",
+        components: [],
+        embeds: [],
+      }).catch((e) => { });
+    };
+    const portFolioMsg = await channel.messages.fetch(messages[2]);
+    await portFolioMsg.edit({
+      content: "Your subscription has ended . Please contact us in our support server to renew it in order to continue using the services !\nThank you."
+    }).catch((e) => { });
+  } else {
+    const channels = config.channel_ids;
+    const messages = config.message_ids;
+    for (i = 0; i <= 3; i++) {
+      const channel = await client.channels.fetch(channels[i]);
+      if (channel) {
+        const message = await channel.messages.fetch(messages[i]);
+        if (message) {
+          message.edit({
+            content: "Your subscription has ended . Please contact us in our support server to renew it in order to continue using the services !\nThank you.",
+            components: [],
+            embeds: [],
+          }).catch((e) => { });
+        };
+      };
+    };
+  };
+};
+
 module.exports = {
   name: 'ready',
   once: true,
@@ -17,6 +54,12 @@ module.exports = {
         }).catch((e) => {
           console.log(e);
         });
+        const config = await config_records.findOne({
+          discord_id: sub.discord_id,
+        }).catch((e) => {
+          console.log(e);
+        });
+        if (config) editEndMsg(config, client).catch((e)=>{});
         await config_records.deleteOne({
           discord_id: sub.discord_id,
         }).catch((e) => {
